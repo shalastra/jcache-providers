5390b8083102cf195fe9d979cdcc3782e1f5e755
Szymon Halastra <szymon.edward.halastra@cern.ch>
2019-05-15 16:30:52 +0200

    Add two more providers

--- jcache-ignite/src/main/java/io/shalastra/ignite/IgniteApplication.java
+++ jcache-ignite/src/main/java/io/shalastra/ignite/IgniteApplication.java
@@ -1,9 +1,9 @@
 package io.shalastra.ignite;
 
-import javax.cache.Cache;
 import javax.cache.CacheManager;
 import javax.cache.Caching;
 
+import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.configuration.CacheConfiguration;
 
@@ -15,20 +15,31 @@ public class IgniteApplication {
     // Get or create a cache manager.
     CacheManager cacheMgr = Caching.getCachingProvider().getCacheManager();
 
-// This is an Ignite configuration object (org.apache.ignite.configuration.CacheConfiguration).
+    // This is an Ignite configuration object (org.apache.ignite.configuration.CacheConfiguration).
     CacheConfiguration<Integer, String> cfg = new CacheConfiguration<>();
 
-// Specify cache mode and/or any other Ignite-specific configuration properties.
+    // Specify cache mode and/or any other Ignite-specific configuration properties.
     cfg.setCacheMode(CacheMode.PARTITIONED);
 
-// Create a cache based on the configuration created above.
-    Cache<Integer, String> cache = cacheMgr.createCache("aCache", cfg);
+    // Create a cache based on the configuration created above.
+    IgniteCache<String, Integer> cache = cacheMgr.createCache("aCache", cfg).unwrap(IgniteCache.class);
 
-// Cache operations
-    Integer key = 1;
-    String value = "11";
-    cache.put(key, "value");
+    // Put-if-absent which returns previous value.
+    Integer oldVal = cache.getAndPutIfAbsent("Hello", 11);
 
-    System.out.println(cache.get(key));
+    // Put-if-absent which returns boolean success flag.
+    boolean success = cache.putIfAbsent("World", 22);
+
+    // Replace-if-exists operation (opposite of getAndPutIfAbsent), returns previous value.
+    oldVal = cache.getAndReplace("Hello", 11);
+
+    // Replace-if-exists operation (opposite of putIfAbsent), returns boolean success flag.
+    success = cache.replace("World", 22);
+
+    // Replace-if-matches operation.
+    success = cache.replace("World", 2, 22);
+
+    // Remove-if-matches operation.
+    success = cache.remove("Hello", 1);
   }
 }
