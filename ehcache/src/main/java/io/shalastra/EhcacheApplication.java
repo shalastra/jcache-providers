d7845ee38ca97d020a025f1390ad685f9a39ac46
Szymon Halastra <szymon.edward.halastra@cern.ch>
2019-06-15 12:23:52 +0200

    Add ehcache transactions

--- ehcache/src/main/java/io/shalastra/EhcacheApplication.java
+++ ehcache/src/main/java/io/shalastra/EhcacheApplication.java
@@ -2,6 +2,8 @@ package io.shalastra;
 
 import java.time.Duration;
 
+import bitronix.tm.BitronixTransactionManager;
+import bitronix.tm.TransactionManagerServices;
 import lombok.extern.slf4j.Slf4j;
 import org.ehcache.Cache;
 import org.ehcache.CacheManager;
@@ -10,16 +12,22 @@ import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.core.Ehcache;
+import org.ehcache.transactions.xa.configuration.XAStoreConfiguration;
+import org.ehcache.transactions.xa.txmgr.btm.BitronixTransactionManagerLookup;
+import org.ehcache.transactions.xa.txmgr.provider.LookupTransactionManagerProviderConfiguration;
 
 @Slf4j
 public class EhcacheApplication {
 
   public static void main(String[] args) {
-    try (CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder().build()) {
+    try (CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+        .using(new LookupTransactionManagerProviderConfiguration(BitronixTransactionManagerLookup.class)).build()) {
       cacheManager.init();
 
       CacheConfiguration<String, String> config = CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, String.class,
           ResourcePoolsBuilder.heap(100))
+          .add(new XAStoreConfiguration("simpleCache"))
           .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofHours(1)))
           .build();
 
@@ -32,6 +40,26 @@ public class EhcacheApplication {
       String value = cache.get("key1");
 
       log.info(value);
+
+      /**
+       * TRANSACTION EXAMPLE
+       */
+      BitronixTransactionManager transactionManager =
+          TransactionManagerServices.getTransactionManager();
+
+      transactionManager.begin();
+      {
+        String anotherValue = cache.get("key2");
+
+        log.info(anotherValue);
+
+        if (anotherValue == "value2") {
+          cache.put("key2", "newValue2");
+        }
+
+        cache.put("key3", "value3");
+      }
+      transactionManager.commit();
     }
   }
 }
